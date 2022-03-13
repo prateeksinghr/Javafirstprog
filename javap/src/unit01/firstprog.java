64bf554d6a902d406ac871002cc5a8dd0d8fb03d
prateek rawat <prateekrawat1800@gmail.com>
2022-03-13 13:31:49 +0530

    first commit.

---
+++ javap/src/unit01/firstprog.java
@@ -0,0 +1,10 @@
+package unit01;
+
+public class firstprog {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("First Java Program");
+		}
+// Eclipse output program!!
+}
