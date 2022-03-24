d3c20ab1ca1341e115e8476706b52fca9f999c3b
prateek rawat <prateekrawat1800@gmail.com>
2022-03-24 20:58:22 +0530

    first commit.

--- javap/src/unit01/P1_Task02_Keywords.java
+++ javap/src/unit01/P1_Task02_Keywords.java
@@ -2,9 +2,23 @@ package unit01;
 
 public class P1_Task02_Keywords {
 
+	class student{
+		int b;
+		
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	         	
+		//Syntax error on token "float", invalid VariableDeclaratorId   
+		int float=2.5f;
+		
+		//student cannot be resolved to a variable
+		student int= new student;
+		
+		}
+	//Syntax error on token "char", Identifier expected after this token    
+	char();
 
 	}
-
-}
+    
+  //Syntax error on tokens, delete these tokens
+  class int();
