75bec86baf52abe4253848eb900ba3bfc95e0f91
prateek rawat <prateekrawat1800@gmail.com>
2022-03-24 21:18:34 +0530

    second commit.

---
+++ javap/src/unit01/P1_Task03_Constant.java
@@ -0,0 +1,17 @@
+package unit01;
+
+public class P1_Task03_Constant {
+	static final double pi=3.14;
+	
+	public static final double PRICE=234.84;
+	
+	public static void main(String[] args) {
+		final int a=20;
+		      System.out.println("Value of Pi="+pi);
+		      System.out.println("a="+a);
+		      System.out.println("Price = Rs."+PRICE);
+		
+
+	}
+
+}
